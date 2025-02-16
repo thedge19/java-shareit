8b8379e48481f723afaa96964aa1bac1c384a55c
thedge19 <paz80@yandex.ru>
2025-02-16 23:53:06 +0300

    fix/ ветка add-controllers#6 - замечания ревьюера#2

--- src/main/java/ru/practicum/shareit/item/storage/InMemoryItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/InMemoryItemStorage.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.item.storage;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
