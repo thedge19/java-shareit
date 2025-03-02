f5445c00ab6a3fec1a201baea49d3d8d3e379274
thedge19 <paz80@yandex.ru>
2025-03-02 18:10:10 +0300

    feat/ ветка add-bookings#2

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,13 +1,8 @@
 package ru.practicum.shareit;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RequiredArgsConstructor
 @SpringBootTest
 class ShareItTests {
-	@Test void contextLoads() {
-
-	}
 }
