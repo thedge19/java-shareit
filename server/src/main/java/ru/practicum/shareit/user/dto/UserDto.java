71875627df57eaf324642851987ce10bce0a01c7
thedge <paz80@yandex.ru>
2025-04-30 02:04:12 +0300

    feat/ add-item-requests-and-gateway#30

--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,13 +1,7 @@
 package ru.practicum.shareit.user.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import ru.practicum.shareit.exception.Marker;
-
-import java.util.Objects;
 
 @Data
 @Builder
