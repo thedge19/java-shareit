647b511845814ce0409459a58a314a27a79dd378
thedge <paz80@yandex.ru>
2025-04-30 09:24:40 +0300

    feat/ add-item-requests-and-gateway#30

--- server/src/test/java/ru/practicum/shareIt/user/UserControllerTest.java
+++ server/src/test/java/ru/practicum/shareIt/user/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest(classes = UserController.class)
 @WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
     @Autowired
