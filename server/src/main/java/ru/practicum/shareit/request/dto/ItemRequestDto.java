b9e7d41979cda07443c6f31443cb07ab82097324
thedge <paz80@yandex.ru>
2025-04-30 13:05:37 +0300

    feat/ add-item-requests-and-gateway#35

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.dto.ItemDto;
@@ -8,13 +9,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemRequestDto {
     private Integer id;
     private String description;
-    private Integer requestorId;
+    private Long requestorId;
     private LocalDateTime created;
     private List<ItemDto> items;
 }
