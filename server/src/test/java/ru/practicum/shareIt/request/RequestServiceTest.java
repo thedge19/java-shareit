b232777ffd43f9792833b657be5838f72901fba4
thedge <paz80@yandex.ru>
2025-04-30 13:08:51 +0300

    feat/ add-item-requests-and-gateway#36

--- server/src/test/java/ru/practicum/shareIt/request/RequestServiceTest.java
+++ server/src/test/java/ru/practicum/shareIt/request/RequestServiceTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.ShareItServer;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
@@ -20,6 +22,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@ContextConfiguration(classes = ShareItServer.class)
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
